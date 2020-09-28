4417c04e42b4d0349d29ad8fe2271933064cea5f
Jitae Lee <jitlee@c2r8s7.42seoul.kr>
2020-09-28 15:53:28 +0900

    memset...ing

---
+++ libft/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char a[20];
+
+	printf("%p",memset(a, 65, sizeof(a)));
+
+	for(int i = 0; i < (sizeof(a)/sizeof(char)); i++)
+	{
+		printf("%c\n", a[i]);
+	}
+	printf("%p",a);
+
+	return 0;
+}
