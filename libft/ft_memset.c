efd6dee8535b88b65a6d1e8e7b692f4a8ded05db
Jitae Lee <jitlee@c2r2s4.42seoul.kr>
2020-09-28 20:14:57 +0900

    ft_memset.c complete

---
+++ libft/ft_memset.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jitlee <jitlee@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/28 15:21:36 by jitlee            #+#    #+#             */
+/*   Updated: 2020/09/28 20:14:27 by jitlee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void *ft_memset(void *b, int c, size_t len)
+{
+	unsigned char	*temp;
+	unsigned int	i;
+	unsigned int	i_len;
+
+	i_len = (unsigned int)len;
+	temp = (unsigned char *)b; 
+	i = 0;
+	while(i < i_len)
+	{
+		*(temp + i) = c;
+		i++;
+	}
+	return (b);
+}
--- libft/main2.c
+++ libft/main2.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-#include <string.h>
+
+void *ft_memset(void *b, int c, size_t len);
 
 int main(void)
 {
 	char a[20];
 
-	printf("%p",memset(a, 65, sizeof(a)));
+	printf("%p",ft_memset(a, 65, sizeof(a)));
 
 	for(unsigned int i = 0; i < (sizeof(a)/sizeof(char)); i++)
 	{
