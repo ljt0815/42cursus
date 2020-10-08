36c4e62db24836874c1b807b4367f5b31be21363
Jitae Lee <jitlee@c4r3s6.42seoul.kr>
2020-10-08 18:44:16 +0900

    memcmp edit

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -6,13 +6,13 @@
 /*   By: jitlee <jitlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/08 18:04:45 by jitlee            #+#    #+#             */
-/*   Updated: 2020/10/08 18:29:43 by jitlee           ###   ########.fr       */
+/*   Updated: 2020/10/08 18:42:13 by jitlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_memcmp(const void *s1, const void *s2, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	unsigned char	*new_s1;
 	unsigned char	*new_s2;
@@ -21,11 +21,10 @@ int ft_memcmp(const void *s1, const void *s2, size_t n)
 	new_s1 = (unsigned char *)s1;
 	new_s2 = (unsigned char *)s2;
 	i = 0;
-
 	while (i < n)
 	{
-		if(new_s1[i] != new_s2[i])
-			return (new_s2[i] - new_s1[i]);
+		if (new_s1[i] != new_s2[i])
+			return (new_s1[i] - new_s2[i]);
 		i++;
 	}
 	return (0);
