288aec7f00f445a0738978a7f09ae1aebfa77f2e
Jitae Lee <jitlee@c4r3s6.42seoul.kr>
2020-10-08 20:59:31 +0900

    strnstr edit

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: jitlee <jitlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/08 20:15:02 by jitlee            #+#    #+#             */
-/*   Updated: 2020/10/08 20:54:48 by jitlee           ###   ########.fr       */
+/*   Updated: 2020/10/08 20:56:40 by jitlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	s_len;
 	size_t	f_len;
-	size_t	n;
 	size_t	i;
 
 	if (needle[0] == 0)
@@ -26,11 +25,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	f_len = ft_strlen(needle);
 	if (s_len < f_len || len < f_len)
 		return (0);
-	if (s_len > len)
-		n = len;
-	else
-		n = s_len;
-	while (i < n && (n - i) >= f_len)
+	while (i < len && (len - i) >= f_len)
 	{
 		if (ft_memcmp(&haystack[i], &needle[0], f_len) == 0)
 			return ((char *)&haystack[i]);
