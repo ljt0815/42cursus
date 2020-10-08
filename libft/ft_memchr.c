cecfb39dde52952cf25cde1d003669dd10541460
Jitae Lee <jitlee@c4r3s6.42seoul.kr>
2020-10-08 16:52:29 +0900

    ft_memchr edit

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: jitlee <jitlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/07 15:35:32 by jitlee            #+#    #+#             */
-/*   Updated: 2020/10/07 17:12:12 by jitlee           ###   ########.fr       */
+/*   Updated: 2020/10/08 16:34:26 by jitlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	find = (unsigned char *)s;
 	while (i < n)
 	{
-		if (*(find + i) == c)
+		if (*(find + i) == (unsigned char)c)
 			return (find + i);
 		i++;
 	}
