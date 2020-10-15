a103bf2a3661654fa6fbe3639d72d982d57181ed
ljt0815 <ljt0815@naver.com>
2020-10-15 20:15:00 +0900

    edit strnstr, add sh

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: jitlee <jitlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/08 20:15:02 by jitlee            #+#    #+#             */
-/*   Updated: 2020/10/08 20:56:40 by jitlee           ###   ########.fr       */
+/*   Updated: 2020/10/15 20:13:34 by jitlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,20 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	size_t	s_len;
-	size_t	f_len;
+	size_t	h_len;
+	size_t	n_len;
 	size_t	i;
 
-	if (needle[0] == 0)
-		return ((char *)haystack);
 	i = 0;
-	s_len = ft_strlen(haystack);
-	f_len = ft_strlen(needle);
-	if (s_len < f_len || len < f_len)
+	h_len = ft_strlen(haystack);
+	n_len = ft_strlen(needle);
+	if (!n_len)
+		return ((char *)haystack);
+	if (h_len < n_len || len < n_len)
 		return (0);
-	while (i < len && (len - i) >= f_len)
+	while (i < len && (len - i) >= n_len)
 	{
-		if (ft_memcmp(&haystack[i], &needle[0], f_len) == 0)
+		if (ft_memcmp(&haystack[i], &needle[0], n_len) == 0)
 			return ((char *)&haystack[i]);
 		i++;
 	}
