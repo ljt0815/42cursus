507a0dcc61988623e480cf814665514f94f3fb1c
Jitae Lee <jitlee@c3r2s1.42seoul.kr>
2020-11-06 21:39:14 +0900

    edit init.sh

--- sh/init.sh
+++ sh/init.sh
@@ -9,3 +9,6 @@ echo "set autoindent" >> ~/.vimrc
 echo "set smartindent" >> ~/.vimrc
 echo "set cindent" >> ~/.vimrc
 echo "set showmatch" >> ~/.vimrc
+echo "let @t = \":Stdheader\\<CR>\"" >> ~/.vimrc
+export MAIL="jitlee@student.42seoul.kr"
+alias cc="gcc -Werror -Wall -Wextra"
