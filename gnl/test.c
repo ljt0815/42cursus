33e58d6da7c8136938f8d604ae2b5ce86a251b1b
Jitae Lee <jitlee@c3r2s5.42seoul.kr>
2020-11-12 03:22:09 +0900

    add test

--- gnl/test.c
+++ gnl/test.c
@@ -5,17 +5,45 @@
 int main(void)
 {
 	int fd;
+	int fd1;
+	int fd2;
 	char *line;
 	int ret;
-
 	ret = 1;
 	fd = open("aaa", O_RDONLY);
-	while (ret > 0)
-	{
-		ret = get_next_line(fd, &line);
-		printf("%s\n", line);
-		free(line);
-	}
+	fd1 = open("bbb", O_RDONLY);
+	fd2 = open("ccc", O_RDONLY);
+
+	ret = get_next_line(fd, &line);
+	printf("fd\n"); 
+	printf("ret : %d\n", ret);
+	printf("%s\n", line);
+	free(line);
+	ret = get_next_line(fd1, &line);
+	printf("fd1\n"); 
+	printf("ret : %d\n", ret);
+	printf("%s\n", line);
+	free(line);
+	ret = get_next_line(fd2, &line);
+	printf("fd2\n"); 
+	printf("ret : %d\n", ret);
+	printf("%s\n", line);
+	free(line);
+	ret = get_next_line(fd, &line);
+	printf("fd\n"); 
+	printf("ret : %d\n", ret);
+	printf("%s\n", line);
+	free(line);
+	ret = get_next_line(fd1, &line);
+	printf("fd1\n"); 
+	printf("ret : %d\n", ret);
+	printf("%s\n", line);
+	free(line);
+	ret = get_next_line(fd2, &line);
+	printf("fd2\n"); 
+	printf("ret : %d\n", ret);
+	printf("%s\n", line);
+	free(line);
 	system("leaks a.out > leaks_result; cat leaks_result | grep leaked && rm -rf leaks_result");
 	return 0;
 }
