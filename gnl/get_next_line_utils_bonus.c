9da8f927e162ed53de3746950fcef4192e1e079c
Jitae Lee <jitlee@c3r1s1.42seoul.kr>
2020-11-13 21:57:06 +0900

    add bonus

---
+++ gnl/get_next_line_utils_bonus.c
@@ -0,0 +1,124 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_util.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jitlee <jitlee@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/06 21:49:48 by jitlee            #+#    #+#             */
+/*   Updated: 2020/11/12 03:10:01 by jitlee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	len;
+
+	i = -1;
+	len = 0;
+	while (src[len] != 0)
+		len++;
+	if (size == 0)
+		return (len);
+	if (len < size)
+	{
+		while (++i < len)
+			dest[i] = src[i];
+		dest[i] = 0;
+		return (len);
+	}
+	else
+	{
+		while (++i < size - 1)
+			dest[i] = src[i];
+		if (size != 0)
+			dest[i] = 0;
+		return (len);
+	}
+	return (0);
+}
+
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != 0)
+		i++;
+	while (src[j] != 0 && i + j + 1 < size)
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	dest[i + j] = 0;
+	while (src[j] != 0)
+		j++;
+	if (i < size)
+		return (i + j);
+	else
+		return (size + j);
+}
+
+char	*ft_strdup(const char *src)
+{
+	int		len;
+	int		i;
+	char	*result;
+
+	len = 0;
+	i = -1;
+	while (src[len])
+		len++;
+	if ((result = (char *)malloc(sizeof(char) * len + 1)) == 0)
+		return (0);
+	while (src[++i])
+		result[i] = src[i];
+	result[i] = 0;
+	return (result);
+}
+
+char	*ft_strjoin(char *s1, char *s2, int s2_start, int s2_end)
+{
+	int		s1_len;
+	int		s2_len;
+	char	*result;
+
+	s1_len = 0;
+	while (s1[s1_len])
+		s1_len++;
+	s2_len = 0;
+	while (s2[s2_len])
+		s2_len++;
+	if ((result = (char *)malloc(s1_len + s2_len + 1)) == 0)
+		return (0);
+	ft_strlcpy(result, s1, s1_len + 1);
+	free(s1);
+	ft_strlcat(result, (s2 + s2_start), s1_len + (s2_end - s2_start) + 1);
+	return (result);
+}
+
+char	*ft_substr(char *s, unsigned int start, size_t len)
+{
+	char			*result;
+	unsigned int	s_len;
+
+	s_len = 0;
+	while (s[s_len])
+		s_len++;
+	if (s_len <= start)
+	{
+		if ((result = (char *)malloc(1)) == 0)
+			return (0);
+		result[0] = 0;
+		return (result);
+	}
+	if ((result = (char *)malloc(len + 1)) == 0)
+		return (0);
+	ft_strlcpy(result, s + start, len + 1);
+	return (result);
+}
