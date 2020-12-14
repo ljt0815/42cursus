b0dcea23fdcf8e4016a5d6802bd26868aa8afb29
Jitae Lee <jitlee@c1r2s3.42seoul.kr>
2020-12-14 17:20:17 +0900

    edit..ing %d

--- printf/print_int.c
+++ printf/print_int.c
@@ -6,24 +6,45 @@
 /*   By: jitlee <jitlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/14 10:37:34 by jitlee            #+#    #+#             */
-/*   Updated: 2020/12/14 15:16:43 by jitlee           ###   ########.fr       */
+/*   Updated: 2020/12/14 17:15:13 by jitlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <stdio.h>
 
 void	proc_zero(t_parse_dat *dat, char *tmp, char *result, int len)
 {
-	if (tmp == 0)
-		return ;
-	ft_memset(result, '0', dat->width);
+	int flag;
+
+	flag = 0;
+	if (dat->width < dat->precision)
+		flag = 1; 
+	if (flag == 1)
+		ft_memset(result, '0', dat->precision + 1);
+	else if (dat->precision == 0)
+		ft_memset(result, '0', dat->width + 1);
+	else
+		ft_memset(result + (dat->width - dat->precision), '0', dat->precision);
 	if (tmp[0] == '-')
 	{
 		result[0] = '-';
 		tmp += 1;
 		len -= 1;
 	}
-	ft_strncpy(result + (dat->width - len), tmp, len);
+	if (flag == 1)
+		ft_strncpy(result + (dat->precision - len), tmp, len);
+	else
+		ft_strncpy(result + (dat->width - len), tmp, len);
+}
+
+char	*alloc_arr(int flag)
+{
+	char *result;
+
+	result = malloc(flag + 1);
+	ft_memset(result, ' ', flag);
+	return (result);
 }
 
 void	print_int(t_parse_dat *dat, va_list *ap, int *rtn)
@@ -31,21 +52,23 @@ void	print_int(t_parse_dat *dat, va_list *ap, int *rtn)
 	char	*result;
 	char	*tmp;
 	int		len;
+	int		flag;
 
 	tmp = ft_itoa(va_arg(*ap, int));
 	len = ft_strlen(tmp);
+	if ((flag = dat->width) < dat->precision)
+		flag = dat->precision;
 	if (dat->width != 0)
 	{
-		result = malloc(dat->width + 1);
-		ft_memset(result, ' ', dat->width);
+		result = alloc_arr(flag);
 		if (dat->flag == FLAG_ZERO)
 			proc_zero(dat, tmp, result, len);
 		else if (dat->flag == FLAG_MINUS)
 			ft_strncpy(result, tmp, len);
 		else
 			ft_strncpy(result + (dat->width - len), tmp, len);
-		write(1, result, dat->width);
-		*rtn += dat->width;
+		write(1, result, flag);
+		*rtn += flag;
 	}
 	else
 	{
