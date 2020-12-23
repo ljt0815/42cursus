b597b511ad11069d35e0199521fae6f53aea2cb9
Jitae Lee <jitlee@c1r1s6.42seoul.kr>
2020-12-23 13:15:55 +0900

    I'm so scared guacamole

--- printf/print_int.c
+++ printf/print_int.c
@@ -6,138 +6,54 @@
 /*   By: jitlee <jitlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/14 10:37:34 by jitlee            #+#    #+#             */
-/*   Updated: 2020/12/21 12:28:06 by jitlee           ###   ########.fr       */
+/*   Updated: 2020/12/23 13:14:42 by jitlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <stdio.h>
 
-void	proc_zero(t_parse_dat *dat, char *tmp, char *result, int len)
-{
-	int minus;
-	int idx;
-
-	minus = 0;
-	idx = 0;
-	if (dat->width <= dat->precision)
-		ft_memset(result, '0', dat->precision + 1);
-	else if (dat->precision == 0)
-		ft_memset(result, '0', dat->width + 1);
-	else
-	{
-		ft_memset(result + (dat->width - dat->precision), '0', dat->precision);
-		idx = dat->width - dat->precision - 1;
-	}
-	if (tmp[0] == '-')
-	{
-		result[idx] = '-';
-		result += 1;
-		tmp += 1;
-		minus = 1;
-	}
-	if (dat->width <= dat->precision)
-		ft_strncpy(result + (dat->precision - len + minus), tmp, len - minus);
-	else
-		ft_strncpy(result + (dat->width - len), tmp, len - minus);
-}
-
-void	proc_minus(t_parse_dat *dat, char *tmp, char *result, int len)
-{
-	int minus;
-
-	minus = 0;
-	if (tmp[0] == '-')
-	{
-		result[0] = '-';
-		result += 1;
-		tmp += 1;
-		minus = 1;
-	}
-	if (dat->precision != 0)
-	{
-		ft_memset(result, '0', dat->precision);
-		ft_strncpy(result + (dat->precision - len + minus), tmp, len - minus);
-	}
-	else
-		ft_strncpy(result, tmp, len - minus);
-}
-
-char	*alloc_arr(t_parse_dat *dat, int len)
+char	*alloc_arr(t_parse_dat *dat, int len, int *read_size)
 {
 	char	*result;
-	int		size;
 
-	if (dat->width == 0 && dat->precision != 0)
-		size = dat->precision;
-	else if (len < dat->width)
-	{
-		if ((dat->width) < dat->precision)
-			size = dat->precision;
-		else
-			size = dat->width;
-	}
-	else if (dat->width < dat->precision)
-		size = dat->precision;
-	else
-		size = len;
-	result = malloc(size + 1);
-	ft_memset(result, ' ', size + 1);
+	*read_size = dat->width;
+	if (dat->width < dat->precision)
+		*read_size = dat->precision;
+	if (len > *read_size)
+		*read_size = len;
+	result = malloc(*read_size + 1);
+	ft_memset(result, ' ', *read_size + 1);
 	return (result);
 }
 
-void	print_flag(char *result, char *tmp, int *rtn, t_parse_dat *dat)
+void	fill_front(char *tmp, char *num, int *read_size)
 {
-	int minus;
-	int size;
+	if (num[0] == '-')
+	{
+		tmp[0] = '-';
+		tmp++;
+		num++;
+		(*read_size)++;
+	}
+	if (dat->flag == FLAG_ZERO)
+	{
+		ft_memset(tmp, '0'
 
-	minus = 0;
-	if ((int)(ft_strlen(tmp)) < dat->width)
-	{
-		if (dat->width <= dat->precision)
-		{
-			size = dat->precision;
-			if (tmp[0] == '-')
-				minus = 1;
-		}
-		else
-			size = dat->width;
-	}
-	else if (dat->width < dat->precision)
-	{
-		size = dat->precision;
-		if (tmp[0] == '-')
-			minus = 1;
-	}
-	else
-		size = ft_strlen(tmp);
-	write(1, result, size + minus);
-	*rtn += (size + minus);
 }
 
 void	print_int(t_parse_dat *dat, va_list *ap, int *rtn)
 {
 	char	*result;
+	char	*num;
 	char	*tmp;
 	int		len;
+	int		read_size;
 
-	tmp = ft_itoa(va_arg(*ap, int));
-	len = ft_strlen(tmp);
-	result = alloc_arr(dat, len);
-	if (dat->width != 0)
-	{
-		if (dat->flag == FLAG_ZERO)
-			proc_zero(dat, tmp, result, len);
-		else if (dat->flag == FLAG_MINUS)
-			proc_minus(dat, tmp, result, len);
-		else
-			ft_strncpy(result + (dat->width - len), tmp, len);
-		print_flag(result, tmp, rtn, dat);
-	}
-	else
-	{
-		proc_else(dat, tmp, result, len);
-		print_else(result, tmp, rtn, dat);
-	}
+	num = ft_itoa(va_arg(*ap, int));
+	len = ft_strlen(num);
+	result = alloc_arr(dat, len, &read_size);
+	fill_front(result, tmp, num, &read_size);
+	
 	free(result);
 }
