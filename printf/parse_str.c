d8fb832dc585a1c227ff1ec51d8891d2c1bdfac9
Jitae Lee <jitlee@c2r2s2.42seoul.kr>
2021-01-01 13:12:38 +0900

    * minus process complete

--- printf/parse_str.c
+++ printf/parse_str.c
@@ -6,7 +6,7 @@
 /*   By: jitlee <jitlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/18 21:19:16 by jitlee            #+#    #+#             */
-/*   Updated: 2021/01/01 12:49:46 by jitlee           ###   ########.fr       */
+/*   Updated: 2021/01/01 13:11:17 by jitlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,8 @@ int		starwidth(char c, va_list *ap, t_parse_dat *dat, int *i)
 	if (c == '*')
 	{
 		dat->width = va_arg(*ap, int);
+		if (dat->width < 0)
+			dat->flag |= FLAG_MINUS;
 		*i += 1;
 		return (1);
 	}
@@ -40,6 +42,11 @@ int		starprecision(char c, va_list *ap, t_parse_dat *dat, int *i)
 	if (c == '*')
 	{
 		dat->precision = va_arg(*ap, int);
+		if (dat->precision < 0)
+		{
+			dat->dot = 0;
+			dat->precision = 0;
+		}
 		*i += 1;
 		return (1);
 	}
