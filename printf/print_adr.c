0922153cfa4905c82725038f984bf84162d12359
Jitae Lee <jitlee@c2r2s2.42seoul.kr>
2021-01-02 20:01:06 +0900

    complete

--- printf/print_adr.c
+++ printf/print_adr.c
@@ -6,7 +6,7 @@
 /*   By: jitlee <jitlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/10 19:31:42 by jitlee            #+#    #+#             */
-/*   Updated: 2021/01/02 18:28:24 by jitlee           ###   ########.fr       */
+/*   Updated: 2021/01/02 19:57:26 by jitlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,29 @@
 
 void	hex_convert(unsigned long tmp, int *rtn)
 {
-	char	*full_addr;
+	char	full_addr[12];
+	char	*idx;
 	int		i;
-	int		addr_len;
 
 	i = 0;
-	addr_len = hex_len((long long)tmp);
-	full_addr = malloc(addr_len);
-	while (i < addr_len)
+	idx = full_addr;
+	while (i < 12)
 	{
-		full_addr[addr_len - i] = (tmp % 16) >= 10 ? \
+		full_addr[11 - i] = (tmp % 16) >= 10 ? \
 							(tmp % 16) + 87 : (tmp % 16) + 48;
 		tmp = tmp / 16;
 		i++;
 	}
+	i = 0;
+	while (i < 3)
+	{
+		if (full_addr[i] == '0')
+			idx++;
+		i++;
+	}
 	write(1, "0x", 2);
-	write(1, full_addr, addr_len);
-	*rtn += addr_len;
+	write(1, idx, 9 + 3 - (idx - full_addr));
+	*rtn += 2 + 9 + 3 - (idx - full_addr);
 }
 
 void	null_proc(t_parse_dat *dat, int *rtn)
