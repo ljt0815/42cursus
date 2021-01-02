e9e6f7833d3dd2a9d05d703487fb7d849eae0482
Jitae Lee <jitlee@c2r2s2.42seoul.kr>
2021-01-02 12:02:23 +0900

    %p complete

--- printf/print_adr.c
+++ printf/print_adr.c
@@ -6,19 +6,17 @@
 /*   By: jitlee <jitlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/10 19:31:42 by jitlee            #+#    #+#             */
-/*   Updated: 2020/12/13 00:50:11 by jitlee           ###   ########.fr       */
+/*   Updated: 2021/01/02 12:01:28 by jitlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	hex_convert(void *addr)
+void	hex_convert(unsigned long tmp, int *rtn)
 {
-	unsigned long	tmp;
-	char			full_addr[9];
-	int				i;
+	char	full_addr[9];
+	int		i;
 
-	tmp = (unsigned long)addr;
 	i = 0;
 	while (i < 9)
 	{
@@ -29,17 +27,40 @@ void	hex_convert(void *addr)
 	}
 	write(1, "0x", 2);
 	write(1, full_addr, 9);
+	*rtn += 11;
+}
+
+void	null_proc(t_parse_dat *dat, int *rtn)
+{
+	int i;
+
+	i = 3;
+	if (dat->flag == FLAG_MINUS)
+		write(1, "0x0", 3);
+	while (i < dat->width)
+	{
+		write(1, " ", 1);
+		i++;
+		*rtn += 1;
+	}
+	if (dat->flag != FLAG_MINUS)
+		write(1, "0x0", 3);
+	*rtn += 3;
 }
 
 void	print_adr(t_parse_dat *dat, va_list *ap, int *rtn)
 {
-	int i;
-
+	int				i;
+	unsigned long	tmp;
+	
 	i = 11;
-	if (dat->width != 0 && dat->width > 11)
+	tmp = (unsigned long)va_arg(*ap, void *); 
+	if (tmp == 0)
+		null_proc(dat, rtn);
+	else if (dat->width != 0 && (dat->width > 11))
 	{
 		if (dat->flag == FLAG_MINUS)
-			hex_convert(va_arg(*ap, void *));
+			hex_convert(tmp, rtn);
 		while (i < dat->width)
 		{
 			write(1, " ", 1);
@@ -47,9 +68,9 @@ void	print_adr(t_parse_dat *dat, va_list *ap, int *rtn)
 			*rtn += 1;
 		}
 		if (dat->flag == 0)
-			hex_convert(va_arg(*ap, void *));
+			hex_convert(tmp, rtn);
 	}
 	else
-		hex_convert(va_arg(*ap, void *));
+		hex_convert(tmp, rtn);
 	*rtn += 11;
 }
