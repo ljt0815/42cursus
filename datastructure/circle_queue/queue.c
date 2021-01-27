6545d395c9147fb88b4991c845915f1ccf86efec
ljt0815 <ljt0815@naver.com>
2021-01-27 11:54:41 +0000

    add queue

---
+++ datastructure/circle_queue/queue.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_QUEUE_SIZE 5
+
+typedef int element;
+typedef struct {
+    int front;
+    int rear;
+    element data[MAX_QUEUE_SIZE];
+} QueueType;
+
+void    error(char *message)
+{
+    fprintf(stderr, "%s\n", message);
+    exit(1);
+}
+
+void    init_queue(QueueType *q)
+{
+    q->rear = 0;
+    q->front = 0;
+}
+
+int     is_empty(QueueType *q)
+{
+    return (q->front == q->rear);
+}
+
+void    queue_print(QueueType *q)
+{
+    int i;
+    
+    printf("QUEUE(front=%d rear=%d) = ", q->front, q->rear);
+    if (!is_empty(q))
+    {
+        i = q->front;
+        do {
+            i = (i + 1) % (MAX_QUEUE_SIZE);
+            printf("%d | ", q->data[i]);
+            if (i == q->rear)
+                break;
+        } while (i != q->front);
+    }
+    printf("\n");       
+}
+
+int     is_full(QueueType *q)
+{
+    return ((q->rear + 1) % MAX_QUEUE_SIZE == q->front);
+}
+
+void    enqueue(QueueType *q, int item)
+{
+    if (is_full(q))
+        error("큐가 포화상태입니다.");
+    q->rear = (q->rear + 1) % MAX_QUEUE_SIZE;
+    q->data[q->rear] = item;
+}
+
+int     dequeue(QueueType *q)
+{
+    if (is_empty(q))
+        error("큐가 공백상태입니다.");
+    q->front = (q->front + 1) % MAX_QUEUE_SIZE;
+    return q->data[q->front];
+}
+
+element peek(QueueType *q)
+{
+    if (is_empty(q))
+        error("큐가 공백상태입니다.");
+    return q->data[(q->front + 1) % MAX_QUEUE_SIZE];
+}
+
+int     main(void)
+{
+    QueueType q;
+    int ele;
+    
+    init_queue(&q);
+    puts("--데이터 추가 단계--");
+    while (!is_full(&q))
+    {
+        printf("정수를 입력하시오: ");
+        scanf("%d", &ele);
+        enqueue(&q, ele);
+        queue_print(&q);
+    }
+    puts("큐는 포화상태일껄 \n");
+    
+    puts("--데이터 삭제 단계--");
+    while (!is_empty(&q))
+    {
+        ele = dequeue(&q);
+        printf("꺼내진 정수: %d \n", ele);
+        queue_print(&q);
+    }
+    puts("큐는 비어있을껄");
+    return 0;
+}
