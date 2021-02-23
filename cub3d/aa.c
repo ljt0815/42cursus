cc9e8717c1092718c94aee9b130fb4b1b29af3b9
jitlee <jitlee@student.42seoul.kr>
2021-02-23 23:04:37 +0900

    linux test complete

--- cub3d/aa.c
+++ cub3d/aa.c
@@ -2,11 +2,39 @@
 #include "libft.h"
 #include "mlx.h"
 
-int main(void)
-{
-	void *mlx_ptr;
 
-	mlx_ptr = mlx_init();
-	printf("%p", mlx_ptr);
-	return 0;
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+void			my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+int	main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1280, 768, "Hello world!");
+	img.img = mlx_new_image(mlx, 1280, 768);// 이미지 instance 생성
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	//이미지의 주소 할당
+	for(int i = 300; i < 700 ; i++) {
+		my_mlx_pixel_put(&img, i, i, 0x00FF0000);// 붉은색 선을 대각선으로 그린다.
+		my_mlx_pixel_put(&img, 300, i, 0x00FF0000);// 붉은색 선을 세로으로 그린다.
+		my_mlx_pixel_put(&img, i, 300, 0x00FF0000);// 붉은색 선을 가로으로 그린다.
+	}
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);//이미지를 윈도우에 올린다.
+	mlx_loop(mlx);
 }
