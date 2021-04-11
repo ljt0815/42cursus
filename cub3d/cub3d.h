0f62f4a0c35c8e54b0e480d92bd67c61bffe504a
Jitae Lee <jitlee@c3r4s4.42seoul.kr>
2021-04-11 10:33:45 +0900

    map no, so complete)

--- cub3d/cub3d.h
+++ cub3d/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: jitlee <jitlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/05 00:45:49 by jitlee            #+#    #+#             */
-/*   Updated: 2021/04/05 02:37:56 by jitlee           ###   ########.fr       */
+/*   Updated: 2021/04/11 09:34:07 by jitlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,21 @@
 
 # include <stdlib.h>
 # include <fcntl.h>
+# include <stdlib.h>
+# include <stdio.h>
 
 typedef struct {
-	int x;
-	int y;
-}		t_r;
+	int		x;
+	int		y;
+}			t_r;
 
 typedef struct {
-	t_r	r;
-}		t_dat;
-int		ft_isdigit(int c);
-int		map_check(char *path, t_dat *dat);
-void	ft_bzero(void *s, size_t n);
+	t_r		r;
+	char	*no;
+	char	*so;
+}	t_dat;
+
+int			ft_isdigit(int c);
+int			map_check(char *path, t_dat *dat);
+void		ft_bzero(void *s, size_t n);
 #endif
--- cub3d/map_check.c
+++ cub3d/map_check.c
@@ -6,18 +6,17 @@
 /*   By: jitlee <jitlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/05 00:33:43 by jitlee            #+#    #+#             */
-/*   Updated: 2021/04/08 19:06:32 by jitlee           ###   ########.fr       */
+/*   Updated: 2021/04/11 09:35:05 by jitlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include "cub3d.h"
-#include <stdio.h>
 
-int		resol_check(char *line, t_dat *dat)
+void	resol_check(char *line, t_dat *dat)
 {
 	if (*(line) != ' ')
-		return (-1);
+		exit(1);
 	while (*(line) == ' ')
 		line++;
 	while (ft_isdigit(*(line)))
@@ -26,7 +25,7 @@ int		resol_check(char *line, t_dat *dat)
 		line++;
 	}
 	if (*(line) != ' ')
-		return (-1);
+		exit(1);
 	while (*(line) == ' ')
 		line++;
 	while (ft_isdigit(*(line)))
@@ -34,29 +33,49 @@ int		resol_check(char *line, t_dat *dat)
 		dat->r.y = dat->r.y * 10 + (*(line) - '0');
 		line++;
 	}
-	return (1);
+}
+
+void	path_check(char *line, t_dat *dat, char news)
+{
+	if (*(line) != ' ')
+		exit(1);
+	while (*(line) == ' ')
+		line++;
+	if (open(line, O_RDONLY) == -1)
+		exit(1);
+	if (news == 'n')
+		dat->no = line;
+	else if (news == 's')
+		dat->so = line;
 }
 
 int		map_check(char *my_path, t_dat *dat)
 {
-	char *line;
-	int fd;
-	int i;
-	int state;
+	char	*line;
+	int		fd;
+	int		state;
 
-	dat += 0;
 	fd = open(my_path, O_RDONLY);
 	while ((state = get_next_line(fd, &line)))
 	{
 		if (state == -1)
 		{
-			write(1, "error", 5);
+			write(1, "Error\n", 6);
 			return (0);
 		}
-		i = 0;
 		if (line[0] == 'R')
 			resol_check(&line[1], dat);
+		if (line[0] == 'N')
+		{
+			if (line[1] == 'O')
+				path_check(&line[2], dat, 'n');
+		}
+		if (line[0] == 'S')
+		{
+			if (line[1] == 'O')
+				path_check(&line[2], dat, 's');
+		}
 	}
-	printf("x = %d\ny = %d", dat->r.y, dat->r.x);
+	printf("x = %d\ny = %d\nNO=%s\nSO=%s\n", dat->r.y, dat->r.x, dat->no, dat->so);
 	return (0);
 }
