f0f1017579dce74509c15b14b80b36ea9dcc0bd2
ljt0815 <ljt0815@naver.com>
2021-04-30 00:19:15 +0900

    mapcpy 0 -> space

--- cub3d/ft_mapcpy.c
+++ cub3d/ft_mapcpy.c
@@ -6,7 +6,7 @@
 /*   By: jitlee <jitlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/19 09:49:37 by jitlee            #+#    #+#             */
-/*   Updated: 2021/04/23 10:52:09 by jitlee           ###   ########.fr       */
+/*   Updated: 2021/04/29 20:07:03 by jitlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*ft_mapcpy(const char *src, t_dat *dat)
 	i = -1;
 	if ((result = (char *)malloc(sizeof(char) * dat->map.y + 1)) == 0)
 		return (0);
-	ft_memset(result, '0', dat->map.y);
+	ft_memset(result, ' ', dat->map.y);
 	result[dat->map.y] = 0;
 	while (src[++i])
 		result[i] = src[i];
