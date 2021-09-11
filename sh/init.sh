a0cf0aac453b1ec7a6b7b5c89d49b37369f7b9f8
Jitae Lee <jitlee@c10r6s3.42seoul.kr>
2021-09-11 15:22:41 +0900

    init.sh modified, Stdheader complete

--- sh/init.sh
+++ sh/init.sh
@@ -11,4 +11,17 @@ echo "set cindent" >> ~/.vimrc
 echo "set showmatch" >> ~/.vimrc
 echo "let @t = \":Stdheader\\<CR>\"" >> ~/.vimrc
 export MAIL="jitlee@student.42seoul.kr"
+mkdir -p ~/.vim/plugin
+cp stdheader.vim ~/.vim/plugin/
+if [ ! -z "$USER" ]
+then
+    USER=`/usr/bin/whoami`
+    export USER
+fi
+
+if [ ! -z "$GROUP" ]
+then
+    GROUP=`/usr/bin/id -gn $user`
+    export GROUP
+fi
 alias cc="gcc -Werror -Wall -Wextra"
