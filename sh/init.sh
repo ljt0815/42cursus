21ee64d274a16778b48615ab9c680ced81929fc2
Jitae Lee <jitlee@c7r3s3.42seoul.kr>
2021-09-14 21:00:53 +0900

    init.sh modified

--- sh/init.sh
+++ sh/init.sh
@@ -25,3 +25,5 @@ then
     export GROUP
 fi
 alias cc="gcc -Werror -Wall -Wextra"
+rm -rf $HOME/.brew && git clone --depth=1 https://github.com/Homebrew/brew $HOME/.brew && echo 'export PATH=$HOME/.brew/bin:$PATH' >> $HOME/.zshrc && source $HOME/.zshrc && brew update
+brew install readline
