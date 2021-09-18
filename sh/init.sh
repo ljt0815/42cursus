d720da6f5701331f2a52a9086e2b8bce9d089866
Jitae Lee <jitlee@c7r4s3.42seoul.kr>
2021-09-18 15:20:42 +0900

    init stdheader delete

--- sh/init.sh
+++ sh/init.sh
@@ -11,19 +11,19 @@ echo "set cindent" >> ~/.vimrc
 echo "set showmatch" >> ~/.vimrc
 echo "let @t = \":Stdheader\\<CR>\"" >> ~/.vimrc
 export MAIL="jitlee@student.42seoul.kr"
-mkdir -p ~/.vim/plugin
-cp stdheader.vim ~/.vim/plugin/
-if [ ! -z "$USER" ]
-then
-    USER=`/usr/bin/whoami`
-    export USER
-fi
-
-if [ ! -z "$GROUP" ]
-then
-    GROUP=`/usr/bin/id -gn $user`
-    export GROUP
-fi
+#mkdir -p ~/.vim/plugin
+#cp stdheader.vim ~/.vim/plugin/
+#if [ ! -z "$USER" ]
+#then
+#    USER=`/usr/bin/whoami`
+#    export USER
+#fi
+#
+#if [ ! -z "$GROUP" ]
+#then
+#    GROUP=`/usr/bin/id -gn $user`
+#    export GROUP
+#fi
 alias cc="gcc -Werror -Wall -Wextra"
 rm -rf $HOME/.brew && git clone --depth=1 https://github.com/Homebrew/brew $HOME/.brew && echo 'export PATH=$HOME/.brew/bin:$PATH' >> $HOME/.zshrc && source $HOME/.zshrc && brew update
 brew install readline
