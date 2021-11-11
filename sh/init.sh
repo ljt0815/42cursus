b1bec405b67d03b13998948ff972c80c46849f9f
Jitae Lee <jitlee@c7r3s4.42seoul.kr>
2021-11-11 15:02:30 +0900

    add cpp00

--- sh/init.sh
+++ sh/init.sh
@@ -25,5 +25,5 @@ export MAIL="jitlee@student.42seoul.kr"
 #    export GROUP
 #fi
 alias cc="gcc -Werror -Wall -Wextra"
-rm -rf $HOME/.brew && git clone --depth=1 https://github.com/Homebrew/brew $HOME/.brew && echo 'export PATH=$HOME/.brew/bin:$PATH' >> $HOME/.zshrc && source $HOME/.zshrc && brew update
-brew install readline
+#rm -rf $HOME/.brew && git clone --depth=1 https://github.com/Homebrew/brew $HOME/.brew && echo 'export PATH=$HOME/.brew/bin:$PATH' >> $HOME/.zshrc && source $HOME/.zshrc && brew update
+#brew install readline
