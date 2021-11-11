01d959ea3bf69aa970200ba4e0729af30140eb86
Jitae Lee <jitlee@c7r3s4.42seoul.kr>
2021-11-11 15:47:06 +0900

    cpp00 ex00 complete

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,14 +6,29 @@
 /*   By: jitlee <jitlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 14:50:23 by jitlee            #+#    #+#             */
-/*   Updated: 2021/11/11 14:51:00 by jitlee           ###   ########.fr       */
+/*   Updated: 2021/11/11 15:45:15 by jitlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int	main(void)
+int	main(int ac, char *av[])
 {
-	std::cout << "hello, world!\n";
+	int	i;
+	int	j;
+
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		i = 0;
+		while (++i < ac)
+		{
+			j = -1;
+			while (av[i][++j])
+				std::cout << (char)std::toupper(av[i][j]);
+		}
+	}
+	std::cout << std::endl;
 	return 0;
 }
