aade0721ef73d8ab610890f21cfb49c8e1811005
Jitae Lee <jitlee@c6r3s8.42seoul.kr>
2022-01-29 09:15:05 +0900

    modified main

--- cpp02/ex02/main.cpp
+++ cpp02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jitlee <jitlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 19:37:15 by jitlee            #+#    #+#             */
-/*   Updated: 2022/01/28 17:50:14 by jitlee           ###   ########.fr       */
+/*   Updated: 2022/01/29 09:14:34 by jitlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,18 @@
 
 int	main(void)
 {
-	Fixed b( 10 );
-	Fixed c( 42.42f );
-	Fixed a( 10 );
-	Fixed const d( 3 );
-	Fixed const e( 1 );
+	Fixed		a;
+	Fixed const	b( Fixed( 5.05f ) * Fixed( 2 ) );
 
-	std::cout << (c < b) << std::endl;
-	std::cout << (c > b) << std::endl;
-	std::cout << (a == b) << std::endl;
-	std::cout << (a != b) << std::endl;
-	std::cout << a + c << std::endl;
-	std::cout << a - c << std::endl;
-	std::cout << a * c << std::endl;
-	std::cout << a / c << std::endl;
-	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
 	std::cout << ++a << std::endl;
-	std::cout << b-- << std::endl;
-	std::cout << --b << std::endl;
-	Fixed const &w = Fixed::max(e, d);
-	std::cout << w << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+
+	std::cout << b << std::endl;
+
+	std::cout << Fixed::max( a, b ) << std::endl;
+
 	return (0);
 }
