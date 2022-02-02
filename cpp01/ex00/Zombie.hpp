ef9ad76849b5c838d79d1febe89db8407ec43374
Jitae Lee <jitlee@c6r4s8.42seoul.kr>
2022-02-02 18:18:06 +0900

    announce change public

--- cpp01/ex00/Zombie.hpp
+++ cpp01/ex00/Zombie.hpp
@@ -6,7 +6,7 @@
 /*   By: jitlee <jitlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/29 23:49:25 by jitlee            #+#    #+#             */
-/*   Updated: 2022/01/29 11:13:10 by jitlee           ###   ########.fr       */
+/*   Updated: 2022/02/02 18:17:38 by jitlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@
 class Zombie {
 	private	:
 		std::string name;
-		void		announce(void);
 
 	public	:
+		void		announce(void);
 		Zombie(void);
 		Zombie(std::string name);
 		~Zombie(void);
