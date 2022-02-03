f5c8814220ccc0f6c111da219f3b407f57a89c6c
Jitae Lee <jitlee@c6r5s6.42seoul.kr>
2022-02-03 20:42:52 +0900

    cpp03 ex00 ing

--- cpp03/ex00/ClapTrap.cpp
+++ cpp03/ex00/ClapTrap.cpp
@@ -6,18 +6,86 @@
 /*   By: jitlee <jitlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 13:07:05 by jitlee            #+#    #+#             */
-/*   Updated: 2022/01/31 13:12:22 by jitlee           ###   ########.fr       */
+/*   Updated: 2022/02/03 20:42:20 by jitlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
+ClapTrap::ClapTrap(void)
+{
+	std::cout << "Default Constructor called" << std::endl;
+	_hp = 10;
+	_ep = 10;
+	_ad = 0;
+}
+
+ClapTrap &ClapTrap::operator=(const ClapTrap &c)
+{
+	if (this != &c)
+	{
+		_name = c.getName();
+		_hp = c.getHp();
+		_ep = c.getEp();
+		_ad = c.getAd();
+	}
+	return (*this);
+}
+
 ClapTrap::ClapTrap(std::string name)
 {
-	this->name = name;
+	_name = name;
+	_hp = 10;
+	_ep = 10;
+	_ad = 0;
 }
 
 void	ClapTrap::attack(std::string const & target)
 {
 	
+}
+
+std::string	ClapTrap::getName(void) const
+{
+	return (name);
+}
+
+int	ClapTrap::getHp(void) const
+{
+	return (hp);
+}
+
+int	ClapTrap::getEp(void) const
+{
+	return (ep);
+}
+
+int	ClapTrap::getAd(void) const
+{
+	return (ad);
+}
+
+void	ClapTrap::setName(const std::string &name)
+{
+	_name = name;
+}
+
+void	ClapTrap::setHp(const int &hp)
+{
+	_hp = hp;
+}
+
+void	ClapTrap::setEp(const int &ep)
+{
+	_ep = ep;
+}
+
+void	ClapTrap::setAd(const int &ad)
+{
+	_ad = ad;
+}
+
+ClapTrap::~ClapTrap(void)
+{
+	std::cout << "Destructor called" << std::endl;
 }
--- cpp03/ex00/ClapTrap.hpp
+++ cpp03/ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: jitlee <jitlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 12:10:56 by jitlee            #+#    #+#             */
-/*   Updated: 2022/01/31 13:06:52 by jitlee           ###   ########.fr       */
+/*   Updated: 2022/02/03 20:42:40 by jitlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,25 @@
 
 class ClapTrap {
 	private	:
-		std::string	name;
-		int	hp;
-		int	ep;
-		int	damage;
+		std::string	_name;
+		int	_hp;
+		int	_ep;
+		int	_ad;
 
 	public	:
+		ClapTrap(void);
 		ClapTrap(std::string name);
+		ClapTrap &operator=(const ClapTrap &c);
+		std::string	ClapTrap::getName(void) const;
+		int		getHp(void) const;
+		int		getEp(void) const;
+		int		getAd(void) const;
+		void	setName(const std::string &name);
+		void	setHp(const int &hp);
+		void	setEp(const int &ep);
+		void	setAd(const int &ad);
 		void	attack(std::string const & target);
 		void	takeDamage(unsigned int amount);
 		void	beRepaired(unsigned int amount);
+		~ClapTrap(void);
 }
