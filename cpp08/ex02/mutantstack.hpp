8b6d5479c771d2880c88c21b9d5dd9ef10de6850
Jitae Lee <jitlee@c6r5s6.42seoul.kr>
2022-02-18 10:07:01 +0900

    ing

--- cpp08/ex02/mutantstack.hpp
+++ cpp08/ex02/mutantstack.hpp
@@ -6,11 +6,18 @@
 /*   By: jitlee <jitlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 02:52:16 by jitlee            #+#    #+#             */
-/*   Updated: 2022/02/18 03:11:30 by jitlee           ###   ########.fr       */
+/*   Updated: 2022/02/18 10:02:40 by jitlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MUTANTSTACK_HPP
 # define MUTANTSTACK_HPP
 
+# include <iostream>
+# include <stack>
 
+template <typename T>
+class	MutantStack	: public std::stack<T> {
+	public	:
+		typedef typename std::stack<
+}
