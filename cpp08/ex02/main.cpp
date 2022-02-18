408327e984ca5efbdcbb8020713700ed4b0d5fe8
Jitae Lee <jitlee@c6r5s6.42seoul.kr>
2022-02-18 12:41:21 +0900

    ing..

---
+++ cpp08/ex02/main.cpp
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jitlee <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/18 11:41:28 by jitlee            #+#    #+#             */
+/*   Updated: 2022/02/18 12:35:45 by jitlee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "mutantstack.hpp"
+
+int main()
+{
+	MutantStack<int> mstack;
+
+	mstack.push(5);
+	mstack.push(17);
+
+	std::cout << mstack.top() << std::endl;
+
+	mstack.pop();
+
+	std::cout << mstack.size() << std::endl;
+
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+
+	MutantStack<int>::stack::container_type::iterator it = mstack.begin();
+	MutantStack<int>::stack::container_type::iterator ite = mstack.end();
+
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+	return 0;
+}
+
--- cpp08/ex02/mutantstack.hpp
+++ cpp08/ex02/mutantstack.hpp
@@ -6,7 +6,7 @@
 /*   By: jitlee <jitlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 02:52:16 by jitlee            #+#    #+#             */
-/*   Updated: 2022/02/18 10:02:40 by jitlee           ###   ########.fr       */
+/*   Updated: 2022/02/18 12:36:07 by jitlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,33 @@
 template <typename T>
 class	MutantStack	: public std::stack<T> {
 	public	:
-		typedef typename std::stack<
-}
+		typename std::stack<T>::container_type::iterator begin(void)
+		{
+			return (this->c.begin());
+		}
+
+		typename std::stack<T>::container_type::iterator end(void)
+		{
+			return (this->c.end());
+		}
+
+		typename std::stack<T>::container_type::reverse_iterator rbegin(void)
+		{
+			return (this->c.rbegin());
+		}
+
+		typename std::stack<T>::container_type::reverse_iterator rend(void)
+		{
+			return (this->c.rend());
+		}
+
+		MutantStack(void) {}
+		MutantStack(const MutantStack &m) { *this = m; }
+		MutantStack& operator=(const MutantStack &m)
+		{
+			std::stack<T>::operator=(m);
+			return (*this);
+		}
+		~MutantStack(void) {}
+};
+#endif
